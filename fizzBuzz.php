4b2e10c08d47ae30b99e9d2f48c2409aff485fcb
Harit <slipknot.opal@gmail.com>
2018-10-17 01:16:25 +0700

    refactor

--- fizzBuzz.php
+++ fizzBuzz.php
@@ -3,22 +3,13 @@
 class FizzBuzz{
     public function say($num)
     {
-        if($num == 1){
-            return "1";
-        }
-        if($num == 2){
-            return "2";
-        }
-        if($num == 4){
-            return "4";
-        }
-        if($num == 7){
-            return "7";
-        }
-
+        $resul = "";
         if($num == 3){
-            return "Fizz";
+            $resul = "Fizz";
+        }else{
+            $resul = $num;
         }
+        return $resul;
     }   
 
 }
