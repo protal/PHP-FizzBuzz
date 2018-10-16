7fbe3208d4b4ac84be036731711b8d5ac275ca11
Harit <slipknot.opal@gmail.com>
2018-10-17 00:53:03 +0700

    add 2 test

--- tests/fizzBuzzTest.php
+++ tests/fizzBuzzTest.php
@@ -7,7 +7,12 @@ class FizzBuzzTest extends TestCase{
     public function testInput1MustBeReturn1()
     {
         $fizzBuzz = new FizzBuzz();
-        $this->assertEquals(1,$fizzBuzz->say(1));
+        $this->assertEquals("1",$fizzBuzz->say(1));
+    }
+    public function testInput2MustBeReturn2()
+    {
+        $fizzBuzz = new FizzBuzz();
+        $this->assertEquals("2",$fizzBuzz->say(2));
     }
 }
 
