06e396c4149a48f4391a0e90a8825bdf90ff344f
Harit <slipknot.opal@gmail.com>
2018-10-17 01:03:21 +0700

    add condition for input 3 return Fizz

--- fizzBuzz.php
+++ fizzBuzz.php
@@ -3,8 +3,17 @@
 class FizzBuzz{
     public function say($num)
     {
-        return "1";
-    }    
+        if($num == 1){
+            return "1";
+        }
+        if($num == 2){
+            return "2";
+        }
+        if($num == 3){
+            return "Fizz";
+        }
+    }   
+
 }
 
 ?>
